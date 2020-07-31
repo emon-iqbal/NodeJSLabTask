b4be8aea8f72a718dcc325bdf3c024f2e83743d2
emon-iqbal <rasjbii@gmai.com>
2020-07-31 17:15:02 +0600

    T2M2F1
    T2M2F1

--- controller/Admin.js
+++ controller/Admin.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
+router.get('/', function(req, res)
+{
 	res.render('Admin/dashboard');
 });
 
+/*router.post('/', function(req, res)
+{
 
+	if(req.body.submit == req.body.password)
+	{
+		res.redirect('/Admin');
+	}
+	else
+	{	
+		res.send('iNVALiD !!PLEASE TRY AGAIN');
+	}
+	
+});*/
 
 module.exports = router;
